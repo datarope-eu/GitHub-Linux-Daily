7e81ff38cd9309d07eeb1f2ff4ac9213344cc364
Datarope EU <42061522+datarope-eu@users.noreply.github.com>
2018-08-10 23:22:26 +0200

    Updated and continued daily-routine.sh

--- Daily-maint-files/daily-routine.sh
+++ Daily-maint-files/daily-routine.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 #
 echo 'Linux Daily Maintenance Routine, in a Shell Script'
+                                                # Try sudo yum {...} on CentOS, Fedora or RHEL
 sudo apt update -y
-sudo apt upgrade -y
+sudo apt upgrade -y                             # not used in yum, leave blank if on CentOS etc.
 sudo apt autoremove -y
+
 
 echo '---'
 echo 'Continuing with WP-CLI commands'
+wp theme status --path=/.../path-to-root/
+wp theme update --path=/.../path-to-root/
+
+wp plugin status --path=/.../path-to-root/
+wp plugin update --all --path=/.../path-to-root/
+
+wp config shuffle-salts                         # Newly introduced in WP-CLI v2.0, providing a fresh sets of salts in wp-config.php
+
+echo "Linux Daily Maintenance Routine: Thanks. That's all!"
 
